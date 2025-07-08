28afb8bbe1a6699b69f5b9c127c37c478555a66d
kanika <155656637+KANlKA@users.noreply.github.com>
2025-07-08 12:49:18 +0530

    Delete Flexi-NFTMarketplace-main directory

--- Flexi-NFTMarketplace-main/prisma/migrations/20250212151749_set_wallet_address_to_optional/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ALTER COLUMN "walletAddress" DROP NOT NULL;
--- Flexi-NFTMarketplace-main/prisma/migrations/20250216173027_add_profile_setup_in_user_model/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "needProfileSetup" BOOLEAN NOT NULL DEFAULT true;
